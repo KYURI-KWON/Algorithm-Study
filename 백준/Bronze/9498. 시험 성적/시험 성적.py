e43210e953807c6961feef421b31240b14eeb267
KYURI-KWON <kyurikwon01@gmail.com>
2026-01-15 17:02:45 +0900

    [Bronze V] Title: 시험 성적, Time: 44 ms, Memory: 32412 KB -BaekjoonHub

---
+++ 백준/Bronze/9498. 시험 성적/시험 성적.py
@@ -0,0 +1,11 @@
+N = int(input())
+if 90 <= N <= 100:
+    print("A")
+elif 79 < N <= 89:
+    print("B")
+elif 69 < N <= 79:
+    print("C")
+elif 59 < N <= 69:
+    print("D")
+else:
+    print("F")
